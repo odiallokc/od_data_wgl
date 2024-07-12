7e1a1ae05f0e77ab3eb9b5ad47fb71a0a11d3d1e
Oumar Diallo <124407878+odiallokc@users.noreply.github.com>
2024-07-11 21:49:51 -0700

    plot update

--- myplot.R
+++ myplot.R
@@ -7,4 +7,8 @@ msleep |>
 
 msleep |> 
   ggplot(aes(sleep_total, bodywt)) +
-  geom_point()
+  geom_point() +
+  labs(
+    title = 'Mamals Sleep Study',
+    subtitle = 'Relationship between body mass and total sleep'
+  )
